d03f835ba5a55d5ecc32a1bd58a74705ff1b8de5
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-03-20 17:24:09 +0100

    start X5

---
+++ X5/5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(void)
+{
+	string name;
+	int age;
+
+	cout << "name age: ";
+	cin >> name >> age;
+	cout << name << ":" << age << endl;
+}
