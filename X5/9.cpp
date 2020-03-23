fc33206de8c824d3b28e43a0573b208e3fab36b6
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-03-23 15:42:14 +0100

    finish X5

--- X5/9.cpp
+++ X5/9.cpp
@@ -1,25 +1,20 @@
 #include <iostream>
 #include <fstream>
-#include <vector>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 
 int main(void)
 {
-	ifstream file;
-	vector<int> nums;
+	ofstream file;
+    srand(time(nullptr)); // use current time as seed for random generator
 
 	file.open("intfile.txt");
 
-	cout << "before read: " << nums.size() << endl;
-	for (int i = 0; i < 1000; i++) {
-		int num;
-		file >> num;
-		cout << "read: " << num << endl;
-		nums.push_back(num);
-	}
-	cout << "read size: " << nums.size() << endl;
+	for (int i = 0; i < 1000; i++)
+		file << rand() << endl;
 
 	file.close();
 }
