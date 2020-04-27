f5a0874da180db1d2ffea3b7924d9b867c10e0af
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-04-27 12:11:24 +0200

    X8: 8: finish

---
+++ X8/8.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main(void)
+{
+	char str[] = "a short string";
+	cout << sizeof(str) << endl;
+	cout << strlen(str) << endl;
+}
