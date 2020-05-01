300c54d9591155c6fcec050155664d167a134bd6
Gioh Kim <gi-oh.kim@cloud.ionos.com>
2020-05-02 00:43:08 +0200

    X8:11: finish
    gkim@laptop:~/tcppl_exercise/X8$ g++ 11.cpp && ./a.out
    aaaa
    bb
    cc
    aa
    bb
    cc
    quit
    aa
    aaaa
    bb
    bb
    cc
    cc
    aa
    aaaa
    bb
    cc

--- X8/11.cpp
+++ X8/11.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 #include <string>
-#include <array>
+#include <vector>
 
 using namespace std;
 
 
-void pr_strings(array<string, 3> months)
+int comp_string(string a, string b)
 {
-	for (int i = 0; i < months.size(); i++)
-		cout << months[i] << endl;
+	int i = 0;
+	for (i = 0; i < a.size() && i < b.size(); i++) {
+		if (a[i] != b[i])
+			break;
+	}
+	return a[i] - b[i];
+}
+
+void sort_words(vector<string>& words)
+{
+	for (int i = 0; i < words.size(); i++) {
+		for (int j = i + 1; j < words.size(); j++) {
+			if (comp_string(words[i], words[j]) > 0) {
+				string tmp = words[i];
+				words[i] = words[j];
+				words[j] = tmp;
+			}
+		}
+	}
+}
+
+vector<string> remove_dup(vector<string>& words)
+{
+	vector<string> ret;
+	
+	for (int i = 0; i < words.size(); i++) {
+		ret.push_back(words[i]);
+		for (int j = i + 1; j < words.size(); j++) {
+			if (words[i] == words[j])
+				i++;
+		}
+	}
+	return ret;
 }
 
 int main(void)
 {
-	array<string, 3> months {"Jan", "Feb", "Mar"};
-	pr_strings(months);
+	vector<string> words;
+	string tmp;
+
+	while (cin >> tmp) {
+		if (tmp == "quit")
+			break;
+		words.push_back(tmp);
+	}
+
+	sort_words(words);
+	for (auto w: words)
+		cout << w << endl;
+
+	vector<string> single_words = remove_dup(words);
+	for (auto w: single_words)
+		cout << w << endl;
 }
